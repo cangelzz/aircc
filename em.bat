acbc03612815bacb0f99e0634c85dada8ed40c0b
cangelzz@gmail.com <cangelzz@gmail.com@42378680-c701-4592-2050-19a6a4fce445>
2011-07-07 05:36:14 +0000

    change logo

--- em.bat
+++ em.bat
@@ -1,1 +1,1 @@
-adt -package -target apk-emulator -storetype pkcs12 -keystore sampleCert.pfx mobile.apk mobile-app.xml mobile.swf assets\icon48.png assets\icon128.png
+adt -package -target apk-emulator -storetype pkcs12 -keystore sampleCert.pfx mobile.apk mobile-app.xml mobile.swf assets\icon128.png
